a090804436e8fb42342e091df5dbc2befee6b135
Vinícius de Sousa Viana <viniciusvsv193@gmail.com>
2024-11-24 08:37:09 -0300

    n sei mais

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -12,15 +12,11 @@ export default function LandingPage(){
     const imagesDisplay = useRef(null);
     const display = useRef(null);
     const imagesContainer = useRef(null);
-    const dynamicBackground = useRef(null);
 
     //Variáveis do slider de imagens
     let initialPos, mousePos, maxPos;
     let percentage = -50, lastPercentage = -50;
     let mouseDown = false;
-
-    //Variáveis do background dinâmico
-    let lastPos = 0;
 
     const [imageName, setImageName] = useState("");
 
@@ -44,7 +40,7 @@ export default function LandingPage(){
             mousePos = event.clientX - initialPos;
             maxPos = window.innerWidth / 2; 
 
-            percentage = Math.max(-100, Math.min(0, (mousePos / maxPos) * 100 + lastPercentage));
+            percentage = Math.max(-75, Math.min(-25, (mousePos / maxPos) * 100 + lastPercentage));
             
             if(display.current){
                 display.current.animate({
@@ -63,34 +59,36 @@ export default function LandingPage(){
             }
         };
 
-        const moveBackground = () => {
-            if(dynamicBackground.current){
-                console.log("Movendo o background");
-
-                const newPos = ((lastPos + window.scrollY) / document.documentElement.scrollHeight) * 300;
-
-                dynamicBackground.current.style.backgroundPosition = `${newPos}% 0%`;
-                
-                lastPos = newPos;
-            }
-        };
-
+        //Observer para detectar quando elementos entram na tela
         const observer = new IntersectionObserver(elements => {
             elements.forEach(element => {
-                element.target.classList.toggle("show", element.isIntersecting);
-            })
-        }, {threshold: 0});
+                    const children = element.target.children;
 
-        const animatedElements = document.querySelectorAll(".hidden");
+                    Array.from(children).forEach((child) => {
+                        child.classList.toggle("show", element.isIntersecting);
+                    });
+            });
+        }, {threshold: 0.5, rootMargin: "-100px"});
+
+        const animatedElements = document.querySelectorAll(".hiddenParent");
         animatedElements.forEach((element) => observer.observe(element));
+
+        //Observer específico para elementos que não se encaixaram no array acima
+        //Observer para o título da seção das áreas da dev
+        const areasTitleObserver = new IntersectionObserver(elements => {
+            elements.forEach(element => {
+                element.target.classList.toggle("show", element.isIntersecting);
+            });
+        }, {threshold: 0.5, rootMargin: "-100px"});
+
+        const areasTitle = document.querySelector(".areasTitle");
+        areasTitleObserver.observe(areasTitle);
 
         if(imagesDisplay.current){
             imagesDisplay.current.addEventListener("pointerdown", savePos);
             imagesDisplay.current.addEventListener("pointerup", releasePosition);
             imagesDisplay.current.addEventListener("pointermove", moveContainer);
         }
-
-        window.addEventListener("scroll", moveBackground);
 
         return () => {
             imagesDisplay.current.removeEventListener("pointerdown", savePos);
@@ -99,7 +97,7 @@ export default function LandingPage(){
 
             animatedElements.forEach((element) => observer.unobserve(element));
 
-            window.removeEventListener("scroll", moveBackground);
+            areasTitleObserver.unobserve(areasTitle);
         };
     }, []);
 
@@ -115,83 +113,84 @@ export default function LandingPage(){
 
     return(
         <>
-            <div className="header">  
-                <img src={mainLogo} alt="mainLogo" className="hidden"/>
+            <div className="header hiddenParent">  
+                <img className="hidden" src={mainLogo} alt="mainLogo"/>
                 <h1 className="hidden">DEVELOPS YOU!</h1>
 
                 <h2 className="hidden">Desenvolvimento de Jogos - UNIFEI</h2>
             </div>
 
             <div className="infoText">
-                <div className="subContainer1">
+                <div className="subContainer1 hiddenParent">
                     <h2 className="hidden">Quem Somos</h2>
                     <p className="hidden">A Dev-U foi criada em 2018 por alunos da UNIFEI pelo interesse comum de desenvolver jogos e evoluir juntos!</p>
                 </div>
 
-                <div className="subContainer2">
+                <div className="subContainer2 hiddenParent">
                     <h2 className="hidden">O que Fazemos</h2>
                     <p className="hidden">Nossa missão conta com o desenvolvimento de jogos, participação em Game Jams, desenvolvimento e pesquisas direcionadas para jogos educativos, e também com a capacitação dos membros para o crescente mercado de jogos no Brasil!</p>
                 </div>
 
-                <div ref={dynamicBackground} className="infoTextBackground"></div>
+                <div className="infoTextBackground"></div>
             </div>
 
             <div className="areas">
-                <h2 className="hidden">Nossas Áreas</h2>
+                <h2 className="areasTitle hidden">Nossas Áreas</h2>
 
-                <div className="areaRight">
-                    <div className="areaText">
-                        <h2 className="hidden">Programação</h2>
-                        <p className="hidden">A área de Programação trabalha para dar vida aos planos desenvolvidos pelas demais áreas. Os programadores transformam uma coleção de ideias inanimadas em um conjunto de dados que por sua vez podem ser interpretados e executados por qualquer tipo de maquina moderna, fazendo disso, então, um jogo digital / videogame.</p>
+                <div className="areaRight hiddenParent">
+                    <div className="areaText hidden">
+                        <h2>Programação</h2>
+                        <p>A área de Programação trabalha para dar vida aos planos desenvolvidos pelas demais áreas. Os programadores transformam uma coleção de ideias inanimadas em um conjunto de dados que por sua vez podem ser interpretados e executados por qualquer tipo de maquina moderna, fazendo disso, então, um jogo digital / videogame.</p>
                     </div>
 
-                    <div className="areaImage">
-                        <img className="hidden" src={PGLogo} alt="PGLogo" />
-                    </div>
-                </div>
-
-                <div className="areaLeft">
-                    <div className="areaImage">
-                        <img className="hidden" src={GDLogo} alt="PGLogo" />
-                    </div>
-
-                    <div className="areaText">
-                        <h2 className="hidden">Game Design</h2>
-                        <p className="hidden">Game Design é a arte de criar uma experiência, visando tornar o jogo interessante e divertido para o jogador. Game Designers criam mecânicas, montam e apresentam o pitch dos games, tratam do balanceamento, planejam como a gameplay vai ser executada e muito mais.</p>
+                    <div className="areaImage hidden">
+                        <img src={PGLogo} alt="PGLogo" />
                     </div>
                 </div>
 
-                <div className="areaRight">
-                    <div className="areaText">
-                        <h2 className="hidden">Audiovisual</h2>
-                        <p className="hidden">A área de Audiovisual é responsável por transmitir para o jogador emoções, ideias e informações. Artistas procuram apelar para os sentidos do jogador com imagens, interfaces de usuário, sprites, modelos 3D, música, efeitos sonoros e dublagem para transmitir a sensação do jogo.</p>
+                <div className="areaLeft hiddenParent">
+                    <div className="areaImage hidden">
+                        <img src={GDLogo} alt="PGLogo" />
                     </div>
 
-                    <div className="areaImage">
-                        <img className="hidden" src={AVLogo} alt="AVLogo" />
+                    <div className="areaText hidden">
+                        <h2>Game Design</h2>
+                        <p>Game Design é a arte de criar uma experiência, visando tornar o jogo interessante e divertido para o jogador. Game Designers criam mecânicas, montam e apresentam o pitch dos games, tratam do balanceamento, planejam como a gameplay vai ser executada e muito mais.</p>
                     </div>
                 </div>
 
-                <div className="areaLeft">
-                    <div className="areaImage">
-                        <img className="hidden" src={GMLogo} alt="GMLogo" />
+                <div className="areaRight hiddenParent">
+                    <div className="areaText hidden">
+                        <h2>Audiovisual</h2>
+                        <p>A área de Audiovisual é responsável por transmitir para o jogador emoções, ideias e informações. Artistas procuram apelar para os sentidos do jogador com imagens, interfaces de usuário, sprites, modelos 3D, música, efeitos sonoros e dublagem para transmitir a sensação do jogo.</p>
                     </div>
 
-                    <div className="areaText">
-                        <h2 className="hidden">Gestão e Marketing</h2>
-                        <p className="hidden">Gestão & Marketing é a área responsável por se comunicar com o público do projeto, organizar eventos, firmar parcerias, fazer divulgação e tesouraria. Os gestores lidam com as questões burocráticas e financeiras do projeto.</p>
+                    <div className="areaImage hidden">
+                        <img src={AVLogo} alt="AVLogo" />
+                    </div>
+                </div>
+
+                <div className="areaLeft hiddenParent">
+                    <div className="areaImage hidden">
+                        <img src={GMLogo} alt="GMLogo" />
+                    </div>
+
+                    <div className="areaText hidden">
+                        <h2>Gestão e Marketing</h2>
+                        <p>Gestão & Marketing é a área responsável por se comunicar com o público do projeto, organizar eventos, firmar parcerias, fazer divulgação e tesouraria. Os gestores lidam com as questões burocráticas e financeiras do projeto.</p>
                     </div>
                 </div>
             </div>
             
             <div ref={imagesDisplay} className="gamesDisplay">
-                <div className="displayTitle">
+
+                <div className="displayTitle hiddenParent">
                     <h2 className="hidden">Nossos Jogos</h2>
                     <p className="hidden">Aqui estão alguns destaques dos muitos jogos que já produzimos</p>
                 </div>
                 
                 <div ref={display} className="display">
-                    <div ref={imagesContainer} className="imagesContainer">
+                    <div ref={imagesContainer} className="imagesContainer hiddenParent">
                         <img 
                             src="https://img.itch.zone/aW1nLzE4NDE5NDg0LnBuZw==/315x250%23c/Y4xZfm.png" 
                             name="Mass Flux: Fuga Espacial" 
@@ -227,14 +226,6 @@ export default function LandingPage(){
                         <img 
                             src="https://img.itch.zone/aW1nLzE0MDAyNDIyLnBuZw==/315x250%23c/G5bLt9.png" 
                             name="AlwayZ Up" 
-                            draggable="false"
-                            onMouseEnter={mouseHoverEnter}
-                            onMouseLeave={mouseHoverLeave}
-                            className="hidden"
-                        />
-                        <img 
-                            src="https://img.itch.zone/aW1nLzE3ODk1NDIzLnBuZw==/315x250%23c/4QbRCR.png" 
-                            name="Schrödinger's Cat" 
                             draggable="false"
                             onMouseEnter={mouseHoverEnter}
                             onMouseLeave={mouseHoverLeave}
