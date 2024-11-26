8c8113b1bf4d1f8462b5eaa2a8948110e0f3d067
ViniciusVsV <viniciusvsv193@gmail.com>
2024-11-26 13:46:55 -0300

    footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -36,6 +36,19 @@ function App() {
       </div>
       
       <LandingPage/>
+
+      <div className="footer">
+        <div className="subContainer1">
+          <h2>ENDEREÇO</h2>
+          <p>Av. BPS, 1303, Bairro Pinheirinho</p>
+          <p>Itajubá- MG</p>
+        </div>
+
+        <div className="subContainer2">
+          <h2>EMAIL DE CONTATO</h2>
+          <p>devunifei@gmail.com</p>
+        </div>
+      </div>
     </>
   )
 }
